e282b59ebdd501cebd84f4390d98796429c826ea
AmauryVdt <amaury.vdt@gmail.com>
2021-10-04 04:47:31 +0200

    premier commit

---
+++ Cognac-Behourd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cognac_Behourd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
